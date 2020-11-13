af3555cb268cc10ff6ab8ca7278c9565cd7f4912
Vlad <vladimir.hraban@codersrank.io>
2020-11-13 14:54:15 +0700

    Some test code for albert workflow

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/vonhraban/alrfred-jira
 
 go 1.15
+
+require (
+	github.com/deanishe/awgo v0.27.1
+	github.com/jason0x43/go-alfred v0.0.0-20200121151032-9ea6c1f3044f // indirect
+	howett.net/plist v0.0.0-20201026045517-117a925f2150 // indirect
+)
---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"github.com/deanishe/awgo"
+	"github.com/deanishe/awgo/update"
+)
+
+var (
+	// Icons
+	updateAvailable = &aw.Icon{Value: "icons/update-available.png"}
+	redditIcon      = &aw.Icon{Value: "icons/reddit.png"}
+	githubIcon      = &aw.Icon{Value: "icons/github.png"}
+	translateIcon   = &aw.Icon{Value: "icons/translate.png"}
+	forumsIcon      = &aw.Icon{Value: "icons/forums.png"}
+	stackIcon       = &aw.Icon{Value: "icons/stack.png"}
+	docIcon         = &aw.Icon{Value: "icons/doc.png"}
+
+	query string
+
+	repo = "vonhraban/alrfred-jira"
+
+	// Workflow stuff
+	wf *aw.Workflow
+)
+
+func init() {
+	wf = aw.New(update.GitHub(repo), aw.HelpURL(repo+"/issues"))
+}
+
+func run() {
+	doSearch()
+}
+
+func main() {
+	wf.Run(run)
+}
